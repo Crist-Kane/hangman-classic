f96e57182e11b9a7dd5b6dbb20942a6644f56de7
feur25 <39668417+feur25@users.noreply.github.com>
2021-12-02 09:49:40 +0100

    Add files via upload

--- test.go
+++ test.go
@@ -10,18 +10,44 @@ import (
 var mot string = ""
 var words = []string{"banc", "exmple"}
 var letter rune
+var r = []rune("test")
 
-func choix() {
+type toi struct {
+	vieactuel int
+	viemax    int
+}
+
+func (moi *toi) init(vieactuel int, viemax int) {
+	moi.vieactuel = vieactuel
+	moi.viemax = viemax
+
+}
+func (moi *toi) choix() {
 	var commande string
 	fmt.Println("Si tu veut recommencer tape fin")
 	fmt.Println("Chose →")
 	fmt.Scan(&commande)
 	switch commande {
-	case letter:
-		if letter == words {
-			fmt.Scan(&letter)
-			charCreation(letter)
-			choix()
+	case string(letter):
+		fmt.Scan(&letter)
+		for i := 0; i < len(r); i++ {
+			if moi.vieactuel > 0 {
+				if letter == r[i] {
+					fmt.Println("Bravo tu a trouver une lettre")
+					time.Sleep(2)
+					moi.choix()
+				} else {
+					if letter != r[i] {
+						fmt.Println("Tu na pas trouver la bonne lettre")
+						moi.vieactuel -= 1
+						if moi.vieactuel <= 0 {
+							fmt.Println("Tu as perdue")
+							os.Exit(0)
+						}
+						moi.choix()
+					}
+				}
+			}
 		}
 
 	case "fin":
@@ -31,12 +57,10 @@ func choix() {
 func random() {
 	rand.Seed(time.Now().Unix())
 	for i := 0; i < len(words); i++ {
-		random := rand.Intn(len(words))
 	}
 }
 func main() {
 	fmt.Println("ceci est un test")
-	choix()
 	random()
 
 }
